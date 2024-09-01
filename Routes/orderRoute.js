115c96e38228cc08bd056fa559096d00b533a50d
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-01 23:39:46 +0200

    add order Route

--- Routes/orderRoute.js
+++ Routes/orderRoute.js
@@ -1,2 +1,23 @@
 const express = require("express");
-const {}
+const orderRouter = express.Router;
+
+const {
+  getAllOrders,
+  createOrder,
+  getOrderById,
+  updateOrder,
+  deleteOrder,
+} = require("../controllers/orderController");
+
+orderRouter
+  .route("/")
+  .get(getAllOrders) //Get all Orders
+  .post(createOrder); //create a new Order
+
+orderRouter
+  .route("/:id")
+  .get(getOrderById) //Get a specific Order by ID
+  .put(updateOrder) // update a specific order by ID
+  .delete(deleteOrder); //Delete a specific order by ID
+
+module.exports = orderRouter;
