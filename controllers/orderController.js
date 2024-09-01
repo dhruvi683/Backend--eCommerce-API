83a6421ae2637c54313a4089f13464f90806e46e
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-01 22:52:11 +0200

    add functionality for GET/orders retrieve a list of orders

---
+++ controllers/orderController.js
@@ -0,0 +1,51 @@
+const Order = require("../schemas/Order");
+const User = require("../schemas/User");
+const Product = require("../schemas/Product");
+
+//Retrieve a list of all orders
+const getAllOrders = async (req, res) => {
+  try {
+    const orders = await Order.find();
+    if (!orders.length) {
+      return res.status(200).json({ message: "No orders found in Database" });
+    } else {
+      return res.status(200).json(orders);
+    }
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+//create a new order
+const createOrder = async (req, res) => {
+  try {
+    const { userId, products, total } = req.body;
+
+    // check if user exists
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    // check if products exist
+    for (const item of products) {
+      const product = await Product.findById(item.productId);
+      if (!product) {
+        return res
+          .status(404)
+          .json({ message: `Product with ID ${item.productId}not found` });
+      }
+    }
+
+    //create the order
+    const order = new Order({
+      userId,
+      products,
+      total,
+    });
+    await order.save();
+    res.status(201).json(order);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+module.exports = { getAllOrders, createOrder };
