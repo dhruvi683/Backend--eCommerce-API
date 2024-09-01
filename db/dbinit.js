fc32aaec15f7cf853658a69e4bf4661238afa878
shital Dalavi <shital.dalavi3@gmail.com>
2024-09-01 20:49:47 +0200

    initialize express application and database connection

--- db/dbinit.js
+++ db/dbinit.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI);
-  console.log(`MongoDB Connected: ${conn.connection.host}`);
+  const conn = await mongoose.connect(process.env.MONGO_URL);
   console.log(`MongoDB Connected: ${conn.connection.name}`.underline.cyan);
 };
 
--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+require("colors");
 const cors = require("cors");
 const connectDB = require("./db/dbinit");
 connectDB();
 
-require("dotenv").config();
-require("colors");
+//middleeware :this should be always be there
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); //this is for form data
 
 const port = process.env.PORT || 8080;
 app.get("/", (req, res) => {
   res.send("welcome to the eCommerce-API");
 });
-app.listen(port, () =>
-  console.log(`Server running on http://localhost:${port}`.bgGreen.black)
-);
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`.bgGreen.black);
+});
